097febe72e00a70b0de37dbf9f47da7eed1b3bbb
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-01 10:03:51 +0200

    update server file

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response, Application } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,6 +7,12 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+app.use(cors());
+
+app.use("/api", () => {});
+
+app
+  .listen(port, () => {
+    console.log(`Server is running at ${port}`);
+  })
+  .on("error", (error) => console.error(error));
