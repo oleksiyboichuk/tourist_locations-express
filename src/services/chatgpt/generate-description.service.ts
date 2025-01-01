66fbd62c59d5eb6ea38aa253091a99c63a5ffe42
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-01 11:16:37 +0200

    add service

--- src/services/chatgpt/generate-description.service.ts
+++ src/services/chatgpt/generate-description.service.ts
@@ -1,1 +1,36 @@
-export const generateDescription = async () => {};
+import { OpenAI } from "openai";
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
+
+interface Params {
+  prompt: string;
+  chatModel: string;
+  content: string;
+}
+
+export const getLocationsDescription = async (params: Params) => {
+  const { prompt, chatModel, content } = params;
+
+  try {
+    const response: any = await openai.chat.completions.create({
+      model: chatModel,
+      messages: [
+        {
+          role: "system",
+          content: content,
+        },
+        { role: "user", content: prompt },
+      ],
+      max_tokens: 4500,
+      temperature: 0.3,
+    });
+
+    return response.choices[0].message.content.trim();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
