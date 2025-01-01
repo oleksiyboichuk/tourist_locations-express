fc8bb371a1e0a7458809ccd3d6d5e2aa01e47e1a
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-01 10:24:13 +0200

    add controller

--- src/routers/location.router.ts
+++ src/routers/location.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { locationController } from "../controllers/location.controller";
 const router: Router = Router();
 
-router.use("/location");
+router.use("/location", locationController);
 
 export const locationRouter: Router = router;
