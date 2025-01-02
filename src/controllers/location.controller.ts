5442d37b94a2f6cf4ffa42d9b4ffedd8283ace8a
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-02 09:49:32 +0200

    update

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -1,1 +1,5 @@
-export const locationController = () => {};
+import { getTouristLocations } from "../services/google/find-locations.service";
+
+export const locationController = async () => {
+  const locations = await getTouristLocations();
+};
