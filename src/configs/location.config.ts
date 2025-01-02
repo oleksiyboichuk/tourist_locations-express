aa3c40665c4b7c325c6813c3eb245f75530738e3
_boy4uk <oleskiy.boichuk.v@gmail.com>
2025-01-02 13:04:35 +0200

    update config

---
+++ src/configs/location.config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    countryId: "e8a8ef62-05aa-4091-9106-17e4b01a2d65",
+    cityId: "eeb49ae3-4ba3-4d4b-9f2a-6a07315a5ab3",
+    categoryId: "test"
+}
--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -2,32 +2,33 @@ import {Request, Response} from "express";
 import {getTouristLocations} from "../services/google.service";
 import {generateDescription, generateTranslation} from "../services/open-ai.service";
 import {saveToExcel} from "../utils/excel.util";
+import {config} from "../configs/location.config";
 
 export async function generateTouristLocations(req: Request, res: Response) {
-    const {translationPrompt, descriptionPrompt, model, cityName, countryId, categoryId, language} = req.body;
+    const {translationPrompt, descriptionPrompt, model, cityName, language} = req.body;
 
     try {
         const locations = await getTouristLocations(cityName, language);
         const result = [];
 
         for (let location of locations) {
-
             const chatParams = {
                 chatModel: model,
                 language
             }
+
             const description = await generateDescription({...chatParams, prompt: descriptionPrompt});
             const translation: any = await generateTranslation({...chatParams, prompt: translationPrompt});
 
             if (description && translation) {
                 const excelParams = {
-                    CountryId: countryId,
-                    CityId: cityName,
+                    CountryId: config.countryId,
+                    CityId: config.cityId,
                     AddressMultiLanguage: translation.address,
                     TitleMultiLanguage: translation.name,
                     DescriptionMultiLanguage: description,
                     Location: location.Location,
-                    CategoryId: categoryId,
+                    CategoryId: config.categoryId,
                 };
 
                 await saveToExcel('tables/interesting-places.xlsx', excelParams);
