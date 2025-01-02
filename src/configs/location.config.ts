c1b71b8c8475bf97e140d51e13f3619014296232
_boy4uk <oleskiy.boichuk.v@gmail.com>
2025-01-02 14:05:09 +0200

    remove redundant

--- src/configs/location.config.ts
+++ src/configs/location.config.ts
@@ -2,32 +2,4 @@ export const locationConfig = {
     countryId: "e8a8ef62-05aa-4091-9106-17e4b01a2d65",
     cityId: "eeb49ae3-4ba3-4d4b-9f2a-6a07315a5ab3",
     categoryId: "test",
-    multiDataFormat: {
-        uk: {
-            en: "Текст опису англійською мовою",
-            uk: "Текст опису українською мовою",
-            fr: "Текст опису французькою мовою",
-            pl: "Текст опису польською мовою",
-            ro: "Текст опису румунською мовою",
-            sl: "Текст опису словенською мовою",
-        },
-        en: {
-            en: "Description text in English",
-            uk: "Description text in Ukrainian",
-            fr: "Description text in French",
-            pl: "Description text in Polish",
-            ro: "Description text in Romanian",
-            sl: "Description text in Slovenian"
-        }
-    },
-    multiDataDescription: {
-        uk: {
-            formatDescription: "Ось формат, якого потрібно притримуватися: ",
-            valueDescription: "Ось значення, які потрібно перекласти: ",
-        },
-        en: {
-            formatDescription: "Here is the format to follow: ",
-            valueDescription: "Here are the values to translate: ",
-        },
-    }
 }
