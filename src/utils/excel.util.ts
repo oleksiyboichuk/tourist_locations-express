20c825c11144c811d0eb02e2cef565df347afeb4
_boy4uk <oleskiy.boichuk.v@gmail.com>
2025-01-02 13:13:53 +0200

    update excel file

--- src/utils/excel.util.ts
+++ src/utils/excel.util.ts
@@ -3,40 +3,60 @@ import fs from 'fs';
 
 import {ExcelParams} from "../models/excel.model";
 
-export async function saveToExcel(filePath: string, newLocation: ExcelParams){
+export async function saveToExcel(existingFile: string, newLocations: ExcelParams){
     let workbook;
     let worksheet;
 
     try {
-        if (fs.existsSync(filePath)) {
-            workbook = xlsx.readFile(filePath);
-            worksheet = workbook.Sheets['Sheet1'];
+        if (fs.existsSync(existingFile)) {
+            workbook = xlsx.readFile(existingFile);
+            worksheet = workbook.Sheets["Sheet1"];
         } else {
             workbook = xlsx.utils.book_new();
             worksheet = xlsx.utils.json_to_sheet([]);
-            xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+            xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
         }
 
-        const range = worksheet['!ref']
-            ? xlsx.utils.decode_range(worksheet['!ref'])
+        let range = worksheet["!ref"]
+            ? xlsx.utils.decode_range(worksheet["!ref"])
             : { s: { r: 0, c: 0 }, e: { r: 0, c: 0 } };
-
         const lastRow = range.e.r + 1;
 
-        const row = [
-            newLocation.CountryId,
-            newLocation.CityId,
-            JSON.stringify(newLocation.AddressMultiLanguage),
-            JSON.stringify(newLocation.TitleMultiLanguage),
-            JSON.stringify(newLocation.DescriptionMultiLanguage),
-            JSON.stringify(newLocation.Location),
-            newLocation.CategoryId,
-        ];
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 0 })] = {
+            t: "s",
+            v: newLocations.CountryId,
+        };
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 1 })] = {
+            t: "s",
+            v: newLocations.CityId,
+        };
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 2 })] = {
+            t: "s",
+            v: JSON.stringify(newLocations.AddressMultiLanguage),
+        };
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 3 })] = {
+            t: "s",
+            v: JSON.stringify(newLocations.TitleMultiLanguage),
+        };
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 4 })] = {
+            t: "s",
+            v: JSON.stringify(newLocations.DescriptionMultiLanguage),
+        };
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 5 })] = {
+            t: "s",
+            v: JSON.stringify(newLocations.Location),
+        };
+        worksheet[xlsx.utils.encode_cell({ r: lastRow, c: 6 })] = {
+            t: "s",
+            v: newLocations.CategoryId,
+        };
 
-        xlsx.utils.sheet_add_aoa(worksheet, [row], { origin: lastRow });
-        workbook.Sheets['Sheet1'] = worksheet;
+        range.e.r = lastRow;
+        range.e.c = 6;
 
-        xlsx.writeFile(workbook, filePath);
+        worksheet["!ref"] = xlsx.utils.encode_range(range);
+
+        xlsx.writeFile(workbook, existingFile);
     } catch (error) {
         throw new Error(`Error saving to Excel: ${error}`);
     }
