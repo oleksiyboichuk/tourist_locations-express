0cb951f65bd04d5640ecd3ab6bf0d8caf8adfa45
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-02 10:46:25 +0200

    update server file

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-import express from "express";
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const server = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running at ${port}`);
+    });
+  } catch (error) {
+    console.error(`Server errors: ${error}`);
+  }
+};
+
+server();
