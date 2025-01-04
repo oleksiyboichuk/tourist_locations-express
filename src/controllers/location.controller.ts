e0da51f50e18234ae1c52fa8bd5244551adcbc08
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-04 22:13:15 +0200

    update controller
    add Promise.all instead async/await

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -1,63 +1,85 @@
-import {Request, Response} from "express";
-import {getTouristLocations} from "../services/google.service";
-import {generateDescription, generateTranslation} from "../services/open-ai.service";
-import {saveToExcel} from "../utils/excel.util";
-import {locationConfig} from "../configs/location.config";
+import { Request, Response } from "express";
+import { getTouristLocations } from "../services/google.service";
+import {
+  generateDescription,
+  generateTranslation,
+} from "../services/open-ai.service";
+import { locationConfig } from "../configs/location.config";
 
-export async function locationController(req: Request, res: Response): Promise<void> {
-    const {cityName, translationPrompt, descriptionPrompt, model} = req.body.params;
+const config = { ...locationConfig };
 
-    if(!cityName ||  !translationPrompt || !descriptionPrompt || !model) {
-        res.status(400).json({message: "Bad request!"});
+export async function locationController(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  const { cityName, translationPrompt, descriptionPrompt, model } =
+    req.body.params;
+
+  if (!cityName || !translationPrompt || !descriptionPrompt || !model) {
+    res.status(400).json({ message: "Bad request!" });
+    return;
+  }
+
+  try {
+    const locations = await getTouristLocations(cityName);
+    const result: any = [];
+
+    locations.length = 5;
+
+    if (!locations || locations.length === 0) {
+      res.status(500).json({ message: "No locations found!" });
+      return;
     }
 
-    try {
-        const locations = await getTouristLocations(cityName);
-        const result = [];
+    const promises = locations.map(async (location: any) => {
+      const params = {
+        chatModel: model,
+        locationName: location.name,
+        language: "uk",
+      };
 
-        locations.length = 10;
+      const descriptionPromise = generateDescription({
+        ...params,
+        prompt: descriptionPrompt,
+        cityName: cityName,
+      });
 
-        if(!locations) {
-            res.status(500).json({message: "No locations found!"});
-        }
+      const translationPromise = generateTranslation({
+        ...params,
+        prompt: translationPrompt,
+        locationAddress: location.address,
+      });
 
-        for (let location of locations) {
-            const params = {
-                chatModel: model,
-                locationName: location.name,
-                language: "uk"
-            }
+      const [description, translation] = await Promise.all([
+        descriptionPromise,
+        translationPromise,
+      ]);
 
-            const description = await generateDescription({...params, prompt: descriptionPrompt, cityName: cityName});
-            const translation: any = await generateTranslation({...params, prompt: translationPrompt, locationAddress: location.address});
+      console.log("description", description);
+      console.log("translation", translation);
 
-            console.log('description', description);
-            console.log('translation', translation);
+      if (!description || !translation) {
+        throw new Error("No description or translation found!");
+      }
 
-            if(!description || !translation) {
-                res.status(500).json({message: "No locations found!"});
-            }
+      const excelParams = {
+        CountryId: config.countryId,
+        CityId: config.cityId,
+        AddressMultiLanguage: JSON.parse(translation).address,
+        TitleMultiLanguage: JSON.parse(translation).name,
+        DescriptionMultiLanguage: JSON.parse(description),
+        Location: location.location,
+        CategoryId: config.categoryId,
+      };
 
-            if (description && translation) {
-                const excelParams = {
-                    CountryId: locationConfig.countryId,
-                    CityId: locationConfig.cityId,
-                    AddressMultiLanguage: JSON.parse(translation).address,
-                    TitleMultiLanguage: JSON.parse(translation).name,
-                    DescriptionMultiLanguage: JSON.parse(description),
-                    Location: location.location,
-                    CategoryId: locationConfig.categoryId,
-                };
+      result.push(excelParams);
+    });
 
-                console.log('excelParams', excelParams);
+    await Promise.all(promises);
 
-                // await saveToExcel("../excel/interesting-places.xlsx", excelParams);
-                result.push(excelParams);
-            }
-        }
-
-        res.status(200).json({data: result});
-    } catch (error) {
-        res.status(500).json({message: "Internal server error!"});
-    }
+    res.status(200).json({ data: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 }
