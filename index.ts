dbaad089c7939293ca0e70e99b7f473da692c1d5
Oleksiy Boichuk <oleskiy.boichuk.v@gmail.com>
2025-01-04 22:28:31 +0200

    connect to db

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import app from "./src/app";
+import mongoose from "mongoose";
 
-
+const mongo_key = process.env.MONGO_API_KEY || "";
 const port = process.env.PORT || 8000;
 
-const server = () => {
+const server = async () => {
   try {
+    await mongoose.connect(mongo_key);
     app.listen(port, () => {
       console.log(`Server is running at ${port}`);
     });
