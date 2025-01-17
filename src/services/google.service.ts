46f1e838325bea19dec84af50dd37b9af8518f24
_boy4uk <oleskiy.boichuk.v@gmail.com>
2025-01-17 17:49:00 +0200

    update

--- src/services/google.service.ts
+++ src/services/google.service.ts
@@ -4,23 +4,26 @@ import { environment } from "../environment";
 const GOOGLE_API_KEY = environment.GOOGLE_API_KEY;
 
 export const searchLocationsByGoogle = async (cityName: string, query: string, next?: string) => {
+    console.log('cityName', cityName);
+    console.log('query', query);
+    console.log('next', next);
     if (!cityName || !query) return null;
 
     const url = `https://maps.googleapis.com/maps/api/place/textsearch/json`;
 
     try {
         const params: Record<string, string> = {
-            query: `${query} ${cityName}`,
+            query: `${query.trim()} ${cityName.trim()}`,
             key: GOOGLE_API_KEY,
             language: "uk",
         };
 
         if (next) {
-            params.pagetoken = next;
+            params.pagetoken = next.trim();
         }
 
         const response = await axios.get(url, { params });
-
+        console.log('response', response.data);
         if (!response || response.status !== 200) return null;
 
         return response.data;
